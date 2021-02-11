c6ed1ca18e52c0c33ca7c9fa1f6a4be96e806056
OliwierStan13 <oliwierstan13@gmail.com>
2021-02-11 10:59:03 +0000

    Updates to Portfolio and Home page

--- Portfolio.js
+++ Portfolio.js
@@ -110,6 +110,149 @@ $(document).ready(function () {
         columnTemplate.strokeOpacity = 1;
         
         }); // end am4core.ready()
+        
+
+
+
+        am4core.ready(function() {
+
+          // Themes begin
+          am4core.useTheme(am4themes_material);
+          am4core.useTheme(am4themes_animated);
+          // Themes end
+          
+          
+          
+          var chart = am4core.create('chartdiv', am4charts.XYChart)
+          chart.colors.step = 2;
+          
+          chart.legend = new am4charts.Legend()
+          chart.legend.position = 'top'
+          chart.legend.paddingBottom = 20
+          chart.legend.labels.template.maxWidth = 95
+          
+          var xAxis = chart.xAxes.push(new am4charts.CategoryAxis())
+          xAxis.dataFields.category = 'category'
+          xAxis.renderer.cellStartLocation = 0.1
+          xAxis.renderer.cellEndLocation = 0.9
+          xAxis.renderer.grid.template.location = 0;
+          
+          var yAxis = chart.yAxes.push(new am4charts.ValueAxis());
+          yAxis.min = 0;
+          
+          function createSeries(value, name) {
+              var series = chart.series.push(new am4charts.ColumnSeries())
+              series.dataFields.valueY = value
+              series.dataFields.categoryX = 'category'
+              series.name = name
+          
+              series.events.on("hidden", arrangeColumns);
+              series.events.on("shown", arrangeColumns);
+          
+              var bullet = series.bullets.push(new am4charts.LabelBullet())
+              bullet.interactionsEnabled = false
+              bullet.dy = 30;
+              bullet.label.text = '{valueY}'
+              bullet.label.fill = am4core.color('#ffffff')
+          
+              return series;
+          }
+          
+          chart.data = [
+              {
+                  category: 'Moderna',
+                  first: 98.32,
+                  second: 126.21
+              },
+              {
+                  category: 'Amazon',
+                  first: 121.76,
+                  second: 140.32
+              },
+              {
+                  category: 'Apple',
+                  first: 136.91,
+                  second: 144.32
+              }
+          ]
+          
+          
+          createSeries('first', 'Price at purchase');
+          createSeries('second', 'Price now');
+          
+          function arrangeColumns() {
+          
+              var series = chart.series.getIndex(0);
+          
+              var w = 1 - xAxis.renderer.cellStartLocation - (1 - xAxis.renderer.cellEndLocation);
+              if (series.dataItems.length > 1) {
+                  var x0 = xAxis.getX(series.dataItems.getIndex(0), "categoryX");
+                  var x1 = xAxis.getX(series.dataItems.getIndex(1), "categoryX");
+                  var delta = ((x1 - x0) / chart.series.length) * w;
+                  if (am4core.isNumber(delta)) {
+                      var middle = chart.series.length / 2;
+          
+                      var newIndex = 0;
+                      chart.series.each(function(series) {
+                          if (!series.isHidden && !series.isHiding) {
+                              series.dummyData = newIndex;
+                              newIndex++;
+                          }
+                          else {
+                              series.dummyData = chart.series.indexOf(series);
+                          }
+                      })
+                      var visibleCount = newIndex;
+                      var newMiddle = visibleCount / 2;
+          
+                      chart.series.each(function(series) {
+                          var trueIndex = chart.series.indexOf(series);
+                          var newIndex = series.dummyData;
+          
+                          var dx = (newIndex - trueIndex + middle - newMiddle) * delta
+          
+                          series.animate({ property: "dx", to: dx }, series.interpolationDuration, series.interpolationEasing);
+                          series.bulletsContainer.animate({ property: "dx", to: dx }, series.interpolationDuration, series.interpolationEasing);
+                      })
+                  }
+              }
+          }
+          
+          }); // end am4core.ready()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         //chart 2
         am4core.ready(function() {
